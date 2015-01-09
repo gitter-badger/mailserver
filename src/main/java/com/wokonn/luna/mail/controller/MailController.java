98c6e1448bd8139f432e0cca05bfb7821e91b5f9
jprasad <jibitesh.prasad@gmail.com>
2015-01-10 03:29:42 +0530

    Annotations of the sterotypes

--- src/main/java/com/wokonn/luna/mail/controller/MailController.java
+++ src/main/java/com/wokonn/luna/mail/controller/MailController.java
@@ -3,5 +3,7 @@ package com.wokonn.luna.mail.controller;
 /**
  * Created by jprasad on 10/01/15.
  */
+
+@Controller
 public class MailController {
 }
--- src/main/java/com/wokonn/luna/mail/service/impl/MailServiceImpl.java
+++ src/main/java/com/wokonn/luna/mail/service/impl/MailServiceImpl.java
@@ -5,5 +5,7 @@ import com.wokonn.luna.mail.service.MailService;
 /**
  * Created by jprasad on 10/01/15.
  */
+
+@Service
 public class MailServiceImpl implements MailService {
 }
